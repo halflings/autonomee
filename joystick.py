a59137943d0945f5094451e3b73c440fa359731f
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-02-24 18:40:31 +0100

    Serial communication with Arduino and joystick

--- joystick.py
+++ joystick.py
@@ -1,6 +1,46 @@
-import sfml as sf
-import time
+#!/usr/bin/env python
 
-if sf.Joystick.isConnected(0):
-    print sf.Joystick.getAxisPosition(0, sf.Joystick.Y)
-    time.sleep(1)
+import pygame
+from time import sleep
+import serial
+import sys
+import os
+#from pygame import joystick, event
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+pygame.init()
+#joystick.init()
+j = pygame.joystick.Joystick(0)
+j.init()
+print 'Initialized Joystick : {}'.format(j.get_name())
+
+
+try:
+    while True:
+
+        sum = 0
+
+        while j.get_axis(1) != 0 and j.get_button(11) != 0:
+            sum += j.get_axis(1)/4
+            sleep(0.02)
+            pygame.event.pump()
+
+
+        if sum != 0:
+            data = "{}".format(abs(int(sum*100)))
+            print "SENT {}".format(data)
+            ser.write(data)
+
+        pygame.event.pump()
+        # for i in range(0, j.get_numaxes()):
+        #     if j.get_axis(i) != 0.00:
+        #         print 'Axis %i reads %.2f' % (i, j.get_axis(i))
+        # for i in range(0, j.get_numbuttons()):
+        #     if j.get_button(i) != 0:
+        #         print 'Button %i reads %i' % (i, j.get_button(i))
+
+        sleep(0.1)
+
+except KeyboardInterrupt:
+    j.quit()
+    ser.exit()
